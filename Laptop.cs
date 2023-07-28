95fe515cfd9641543928ed9eae6191c80033e679
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-28 15:15:10 +0530

    first commit

---
+++ Laptop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment18
+{
+    public class Laptop: IConnectable, IRechargeble, IDisplayable
+    {
+        public string Model { get; set; }
+        public bool IsConnected { get; private set; }
+        public int BatteryLevel { get; private set; }
+
+        public Laptop(string model)
+        {
+            Model = model;
+            IsConnected = false;
+            BatteryLevel = 0;
+        }
+
+        public bool Connect()
+        {
+            IsConnected = true;
+            Console.WriteLine($"Laptop {Model} is now connected.");
+            return true;
+        }
+
+        public void Charge(int minutes)
+        {
+            BatteryLevel += minutes;
+            Console.WriteLine($"Laptop {Model} is charged for {minutes} minutes. Battery level: {BatteryLevel}%");
+        }
+
+        public string Display()
+        {
+            return $"Laptop {Model}: Connected: {IsConnected}, Battery Level: {BatteryLevel}%";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone smartphone = new Smartphone("Galaxy S22 Ultra");
+            smartphone.Connect();
+            smartphone.Charge(60);
+            Console.WriteLine(smartphone.Display());
+
+            Console.WriteLine();
+
+            Laptop laptop = new Laptop("Lenovo NotePad 13");
+            laptop.Connect();
+            laptop.Charge(120);
+            Console.WriteLine(laptop.Display());
+            Console.ReadKey();
+        }
+    }
+}
